6ac2457965d53aff39d42062051fff538144f438
Devill00 <118428480+Devill00@users.noreply.github.com>
2023-07-11 23:10:25 +0530

    Updates

---
+++ precache-manifest.f5c8b4236bf5fbe163a548df067b3a56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aac31d7d722a58f54c57",
+    "url": "/Text-Utils/static/css/main.5d678aab.chunk.css"
+  },
+  {
+    "revision": "aac31d7d722a58f54c57",
+    "url": "/Text-Utils/static/js/main.aac31d7d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Text-Utils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Text-Utils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "af7ba7e700011b21addd",
+    "url": "/Text-Utils/static/js/runtime~main.af7ba7e7.js"
+  },
+  {
+    "revision": "150b3fa2a598046c202b6bc90a921694",
+    "url": "/Text-Utils/index.html"
+  }
+];
